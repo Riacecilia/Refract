f198cca648fbba76943c02b24cf086b1a375865f
Riacecilia <211316691+Riacecilia@users.noreply.github.com>
2025-08-05 15:10:04 +0200

    create sidebar structure

---
+++ refract-capstone/docs/getting-started/getting-started.md
@@ -0,0 +1,11 @@
+## Introduction and Overview 
+What is Refract?
+
+Key Features
+
+Comparison with other frameworks 
+
+
+##  Installation
+
+## Quick Start 
--- refract-capstone/docusaurus.config.js
+++ refract-capstone/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Refract',
+  tagline: 'Stream your UI to perfection',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -81,7 +81,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Refract',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',

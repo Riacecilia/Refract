81edd99b86ad65a81561d65cd7db31af0f979c6a
Riacecilia <211316691+Riacecilia@users.noreply.github.com>
2025-08-28 10:53:46 +0200

    deleted blog files, deleted blog and tutorials from the navbar, removed link to docs/intro in the config file

--- refract-capstone/docusaurus.config.js
+++ refract-capstone/docusaurus.config.js
@@ -88,13 +88,6 @@ const config = {
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
             position: 'right',
@@ -108,8 +101,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Introduction',
+                to: '/docs/Introduction',
               },
             ],
           },

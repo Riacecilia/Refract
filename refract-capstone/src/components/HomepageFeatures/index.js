f56e5e30122dacdbb807abc4998e14cfb0ff7934
Riacecilia <211316691+Riacecilia@users.noreply.github.com>
2025-09-02 14:52:59 +0200

    added benefit driven copy on homepage

--- refract-capstone/src/components/HomepageFeatures/index.js
+++ refract-capstone/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Build Fast',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Refract's minimalist API and simple core concepts let you write component logic quickly, getting features built without a steep learning curve..
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Stay Focused',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The isolated lens provided to each component allows you to manage local state and effects without worrying about the rest of your application.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Code Less',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        The declarative, reactive system automatically updates the UI for you, eliminating the need for manual DOM manipulation and reducing boilerplate code.
       </>
     ),
   },
